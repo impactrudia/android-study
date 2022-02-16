e52e3e738dd75099398f5a8d61ca4aba5e7a7074
impactrudia <impactyoung@gmail.com>
2022-02-16 16:08:26 +0900

    feat : initial project

---
+++ InventoryAppAlone01/app/src/main/java/impactrudia/aop/inventoryappalone01/MainActivity.kt
@@ -0,0 +1,11 @@
+package impactrudia.aop.inventoryappalone01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

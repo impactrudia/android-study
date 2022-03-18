58ba2c125b086823a5900eb25bcd8d39f78a2839
impactrudia <impactyoung@gmail.com>
2022-03-18 23:40:14 +0900

    [코드랩] 인터넷 가져오기 - GridView, BindingAdapter (#22)
    * feat : Update ViewModel
    
    * feat : Create a binding adapter and call Glide
    
    - json에서 받아온 이미지 주소는 스키마가 http다. 그렇게 이미지를 로드할 경우, 보안정책상 404 page not found가 뜬다. 따라서 https로 변환함.
    
    * update : the layout and fragment binding check
    
    * refactor : bindingAdapater 코드 줄임
    
    * feat : loading and error images
    
    * feat : Display a grid of images with a RecyclerView
    
    - '===' 연산자는 참조값까지 같은 경우를 말한다.
    - adapter 에 bind메소드에서 'executePendingBindings()'는 속성을 넣은 후에 업데이트를 즉시 실행시키는 원인이다.
    - bindingAdapter에 리스트데이터 바로 넣게되니 코드가 줄여짐. 다만 확장성은 아쉬움.
    
    * feat : Add error handling in RecyclerView

--- CodeLabs/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ CodeLabs/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,3 +17,51 @@
 
 package com.example.android.marsrealestate
 
+import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.MarsApiStatus
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
+
+@BindingAdapter("imageUrl")
+fun ImageView.bindImage(imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(this.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )
+            .into(this)
+    }
+}
+
+@BindingAdapter("listData")
+fun RecyclerView.bindRecyclerView(data: List<MarsProperty>?) {
+    val adapter = adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun ImageView.bindStatus(status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            visibility = View.GONE
+        }
+    }
+}

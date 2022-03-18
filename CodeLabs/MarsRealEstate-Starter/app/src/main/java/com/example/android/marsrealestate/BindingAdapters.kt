f65ec9353e6437904e6439df6aa2fe9759939b8b
impactrudia <impactyoung@gmail.com>
2022-03-18 19:44:59 +0900

    feat : Create a binding adapter and call Glide
    - json에서 받아온 이미지 주소는 스키마가 http다. 그렇게 이미지를 로드할 경우, 보안정책상 404 page not found가 뜬다. 따라서 https로 변환함.

--- CodeLabs/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ CodeLabs/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,3 +17,17 @@
 
 package com.example.android.marsrealestate
 
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imageUrl")
+fun bindImage(imageView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imageView.context)
+            .load(imgUri)
+            .into(imageView)
+    }
+}

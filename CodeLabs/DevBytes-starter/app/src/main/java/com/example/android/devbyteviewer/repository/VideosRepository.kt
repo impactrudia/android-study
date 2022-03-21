ab9c7002bcfc17a3ea41f6533284ceb0902010db
impactrudia <impactyoung@gmail.com>
2022-03-22 04:56:24 +0900

    feat : Retrieve data from the database

--- CodeLabs/DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ CodeLabs/DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,17 +16,25 @@
 
 package com.example.android.devbyteviewer.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.example.android.devbyteviewer.database.VideoDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.example.android.devbyteviewer.network.DevByteNetwork
 import com.example.android.devbyteviewer.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 
-class VideoRepository(private val database: VideoDatabase){
+class VideoRepository(private val database: VideoDatabase) {
 
-    suspend fun refreshVideos(){
-        withContext(Dispatchers.IO){
+    val videos: LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
             Timber.d("refresh videos is called")
             val playlist = DevByteNetwork.devbytes.getPlaylist()
             database.videoDao.insertAll(playlist.asDatabaseModel())
